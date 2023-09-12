353e6388d01633acd08e06f5bc1a9d1d2f4a3bf2
rithika9848 <144332330+rithika9848@users.noreply.github.com>
2023-09-11 22:03:02 -0700

    Create IfExample.java

---
+++ IfExample.java
@@ -0,0 +1,9 @@
+public class IfExample{
+    public static void main(String args[]){
+        int x=10;
+        int y=12;
+        if(x+y>20){
+            System.out.println("x+y is>20");
+        }
+    }
+}
